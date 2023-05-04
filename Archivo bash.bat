c0f504eb6fbb84de4775c23ba5f865997a4909f2
Frank Mansilla <frankmansilla123a@gmail.com>
2023-05-03 21:21:14 -0300

    Nuevo Commit Desde ARCHIVO BAT

---
+++ Archivo bash.bat
@@ -0,0 +1,17 @@
+git checkout branch-prueba
+
+git branch
+
+git add .
+
+git commit -m "Nuevo Commit Desde ARCHIVO BAT"
+
+git push origin branch-prueba
+
+git checkout main
+
+git branch
+
+git merge branch-prueba
+
+git push origin main
